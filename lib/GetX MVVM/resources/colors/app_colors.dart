297ebc76a50e4f556790e96d37e2fe2eb7dd351d
ashhad25 <ashhadahmed72@gmail.com>
2025-05-26 14:38:40 +0500

    Book Buddy - Colors Used In App

---
+++ lib/GetX MVVM/resources/colors/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color transparent = Colors.transparent;
+  static const Color blackColor = Color(0xff000000);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color primaryColor = Color(0xFF1A2980);
+  static const Color primaryButtonColor = Color(0xFF1A2980);
+  static const Color secondaryButtonColor = Color(0xffFC3F5B);
+  static const Color redColor = Color(0xffFC3F5B);
+  static const Color primaryTextColor = Color(0xff000000);
+  static const Color secondaryTextColor = Color(0xff444648);
+  static const Color selectedIconColor = Colors.white;
+  static const Color unSelectedIconColor = Colors.white70;
+}
