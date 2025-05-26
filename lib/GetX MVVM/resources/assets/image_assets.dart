dcb0d61cc0b2c94fa464275811bbb9d3dfd5f85c
ashhad25 <ashhadahmed72@gmail.com>
2025-05-26 14:36:14 +0500

    Book Buddy - Image Asset Defined

---
+++ lib/GetX MVVM/resources/assets/image_assets.dart
@@ -0,0 +1,5 @@
+class ImageAssets {
+  static const String splashScreen = 'assets/images/splash.jpg';
+  static const String bookMockup = 'assets/images/book_mockup.png';
+  static const String messageIcon = 'assets/icons/message.svg';
+}
