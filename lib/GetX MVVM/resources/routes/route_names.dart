c7041e5223f838091947219c679f79031668f241
ashhad25 <ashhadahmed72@gmail.com>
2025-05-26 14:47:14 +0500

    Book Buddy - Route Names

---
+++ lib/GetX MVVM/resources/routes/route_names.dart
@@ -0,0 +1,8 @@
+class RouteNames {
+  static const String splash = '/';
+  static const String loginView = '/login_view';
+  static const String homeView = '/home_view';
+  static const String registerView = '/register_view';
+  static const String bookDetail = '/book_detail';
+  static const String userProfile = '/user_profile';
+}
