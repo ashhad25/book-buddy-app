1042c20d455312c5400a1fdb9475cc0ef8f5207b
ashhad25 <ashhadahmed72@gmail.com>
2025-05-26 14:34:51 +0500

    Book Buddy - App Urls

---
+++ lib/GetX MVVM/resources/app urls/app_urls.dart
@@ -0,0 +1,12 @@
+class AppUrls {
+  static const String baseUrl = 'https://reqres.in';
+  static const String loginApi = '$baseUrl/api/login';
+
+  static const String userListApi =
+      'https://webhook.site/47c4fffd-ec8b-48e2-8745-7933763d81e5';
+
+  // static const String bookListApi =
+  //     "https://api.apileague.com/search-books?api-key=4525116b091f45e3bf6798967471ee0a";
+
+  static const String bookListApi = "https://gutendex.com/books/";
+}
